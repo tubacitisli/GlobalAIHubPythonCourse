3d509ba571dd74513204a4106839b38c97df6ecc
tubacitisli <82044869+tubacitisli@users.noreply.github.com>
2021-04-09 02:35:04 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -8,3 +8,18 @@ a = myList [:5]
 b = myList [5:]
 
 print ("My New List: ", b + a)
+
+
+#Answer 2
+
+n = int (input("Please enter number: " ))
+
+if n < 10:
+
+    print("Even Numbers: \n ")
+
+    for i in range(0, n+1):
+        
+        if i % 2 == 0:
+        
+          print(i, end = " ")
